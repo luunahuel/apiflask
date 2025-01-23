907caa0fef53f28aabf48d22697f2f65f9004b85
luunahuel <68201031+luunahuel@users.noreply.github.com>
2025-01-23 19:45:42 +0000

    recopilo idurl

--- api.py
+++ api.py
@@ -7,4 +7,15 @@ app = Flask(__name__)
 # Definir una ruta GET para la raíz
 @app.route('/')
 def hello_world():
-    return 'Hello!'
+    return 'BUENAS!! es el Punto de entrada '
+
+# Definir una ruta GET para obtener datos del usuario
+@app.route("/users/<user_id>")
+def get_user(user_id):
+    user = {"id": user_id, "name": "test", "telefono": "115603809"}
+    # Obtener parámetros de consulta
+    query = request.args.get("query")
+    if query:
+        user["query"] = query
+        return jsonify(user), 200
+    return jsonify(user), 200
